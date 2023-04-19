5aebe76f17a0981de0cb854d6a6974aa8e3797fa
wolfbeark <wolfbeark0327@gmail.com>
2023-04-20 06:30:50 +0900

    antd 기본 적용

--- src/main/index.js
+++ src/main/index.js
@@ -23,46 +23,42 @@ function MainPage() {
 
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map((product, i) => {
-            return (
-              <Link className="product-link" to={`/products/${i}`} key={i}>
-                <div className="product-card">
-                  <div>
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map((product, i) => {
+          return (
+            <Link
+              className="product-link"
+              to={`/products/${product.id}`}
+              key={i}
+            >
+              <div className="product-card">
+                <div>
+                  <img
+                    className="product-img"
+                    //src="/images/products/keyboard1.jpg"
+                    src={product.imageUrl}
+                  />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
                     <img
-                      className="product-img"
-                      //src="/images/products/keyboard1.jpg"
-                      src={product.imageUrl}
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
                     />
-                  </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <div className="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                      />
-                      <span>{product.seller}</span>
-                    </div>
+                    <span>{product.seller}</span>
                   </div>
                 </div>
-              </Link>
-            );
-          })}
-        </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,46 @@
+import axios from "axios";
+import "./index.css";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        `https://b09b40f8-6e33-49ab-8e47-7524eb07ba69.mock.pstmn.io/products/${id}`
+      )
+      .then(function (res) {
+        console.log(res);
+        setProduct(res.data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }, []);
 
-  return <h1>product {id}</h1>;
+  if (product === null) {
+    return <h1>상품 정보를 받는 중...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
